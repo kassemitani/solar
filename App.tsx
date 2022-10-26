6cfaacec968002b6241eb9a8d676251cb37cbe06
Kassem Itani <kassem.itani@Gmail.com>
2022-10-26 22:06:35 +0300

    Add expo project

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomeScreen from './app/screens/HomeScreen';
+import { SafeAreaView,Text } from 'react-native';
+
+const App = () => (
+  <SafeAreaView>
+     <HomeScreen/>
+  </SafeAreaView>
+) 
+
+export default App
